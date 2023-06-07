7950e9f5f8188b0f384e299ef195966ba1102d16
Ksenia <xeniamishakova@gmail.com>
2023-06-07 13:55:13 +0200

    try

--- homework.py
+++ homework.py
@@ -463,6 +463,29 @@ hello_user()
 
 
 
+def discounted(price, discount, max_discount=20):
+    try:
+        price = float(price)
+        discount = float(discount)
+        max_discount = int(max_discount)
+    except (ValueError, TypeError):
+        raise ValueError('Некорректные аргументы')
+
+    price = abs(price)
+    discount = abs(discount)
+    max_discount = abs(max_discount)
+    
+    if max_discount >= 100:
+        raise ValueError('Слишком большая максимальная скидка')
+
+    if discount >= max_discount:
+        return price
+    else:
+        return price - (price * discount / 100)
+
+
+
+
 
 
 
