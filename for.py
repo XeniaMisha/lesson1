76cc1f0c63da3060638fb224c7e06fd7eb0db892
Ksenia <xeniamishakova@gmail.com>
2023-06-22 17:37:55 +0200

    slice

--- for.py
+++ for.py
@@ -49,3 +49,40 @@ for number in numbers:
 
 numbers = [number**3 for number in numbers]
 print(numbers)
+
+
+my_fav_pizzas = ['pepperoni', 'four cheeses', 'hawaiian', 'margarita']
+for pizza in my_fav_pizzas:
+    print('I like ', pizza)
+print('The first three items in the list are:', my_fav_pizzas[0:3])
+
+
+my_fav_pizzas = ['pepperoni', 'four cheeses', 'hawaiian', 'margarita']
+for pizza in my_fav_pizzas:
+    print('I like ', pizza)
+print('Two items from the middle of the list are:', my_fav_pizzas[1:3])
+
+
+my_fav_pizzas = ['pepperoni', 'four cheeses', 'hawaiian', 'margarita']
+for pizza in my_fav_pizzas:
+    print('I like ', pizza)
+print('The last three items in the list are:', my_fav_pizzas[-3:])
+
+
+animals = ['dog', 'cat', 'turtle', 'rabbit']
+print(animals)
+
+friend_animals = animals[:]
+print(friend_animals)
+
+animals.append('humster')
+friend_animals.append('snake')
+
+print('My animals are:')
+for my_animal in animals:
+    print(my_animal)
+
+print('The animals of my friend are:')
+for him_animals in friend_animals:
+    print(him_animals)
+
